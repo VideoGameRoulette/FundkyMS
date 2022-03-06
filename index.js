f7d0d9bfefc73e5c4ed38cb36ced50b0c7c64995
unknown <videogameroulette@yahoo.ca>
2022-03-06 17:44:32 -0500

    id param added to routes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config;
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const axios = require('axios');
@@ -9,13 +9,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/polls', async (req, res) => {
+app.get('/polls/:id', async (req, res) => {
     try {
         const polls = await axios({
             method: 'get',
-            url: `https://live.dev.fundky.com/en/api/clanfight/list/${process.env.FUNDKY_ID}?showOptionsCount=true&showDescription=true&showOptions=true`,
+            url: `https://live.dev.fundky.com/en/api/clanfight/list/${req.params.id}?showOptionsCount=true&showDescription=true&showOptions=true`,
             responseType: 'json',
         }).then(response => {
+            console.log(response);
             res.json({ polls: response.data.results || [] });
         });
     }
@@ -24,11 +25,11 @@ app.get('/polls', async (req, res) => {
     }
 });
 
-app.get('/milestones', async (req, res) => {
+app.get('/milestones/:id', async (req, res) => {
     try {
         const milestones = await axios({
             method: 'get',
-            url: `https://live.dev.fundky.com/en/api/incentives/list/${process.env.FUNDKY_ID}`,
+            url: `https://live.dev.fundky.com/en/api/incentives/list/${req.params.id}`,
             responseType: 'json',
         }).then(response => {
             res.json({ milestones: response.data.results });
@@ -39,11 +40,11 @@ app.get('/milestones', async (req, res) => {
     }
 });
 
-app.get('/donations/total', async (req, res) => {
+app.get('/donations/total/:id', async (req, res) => {
     try {
         const donations = await axios({
             method: 'get',
-            url: `https://live.dev.fundky.com/en/api/donations/sum/${process.env.FUNDKY_ID}`,
+            url: `https://live.dev.fundky.com/en/api/donations/sum/${req.params.id}`,
             responseType: 'json',
         }).then(response => {
             res.json({ current: response.data });
@@ -54,11 +55,11 @@ app.get('/donations/total', async (req, res) => {
     }
 });
 
-app.get('/donations/list', async (req, res) => {
+app.get('/donations/list/:id', async (req, res) => {
     try {
         const donations = await axios({
             method: 'get',
-            url: `https://live.dev.fundky.com/en/api/donations/list/${process.env.FUNDKY_ID}?showAnonymous=true`,
+            url: `https://live.dev.fundky.com/en/api/donations/list/${req.params.id}?showAnonymous=true`,
             responseType: 'json',
         }).then(response => {
             res.json({ donations: response.data.results || [] });
