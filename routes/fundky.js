d9147f3556f799e2393548a64fd580983a82f969
unknown <videogameroulette@yahoo.ca>
2022-03-10 22:22:28 -0500

    poll sort method added for setting background

--- routes/fundky.js
+++ routes/fundky.js
@@ -1,18 +1,25 @@
 const axios = require('axios');
 
 module.exports = (app) => {
-    const API = process.env.FUNDKY_URL || 'https://live.dev.fundky.com/en/api';
+    const API = process.env.FUNDKY_URL || ' https://fundky.live/en/api';
 
     app.get('/api/polls/:id', async (req, res) => {
+        let pollid = req.query.pollid || null;
+        let filtered = null;
         try {
             const polls = await axios({
                 method: 'get',
                 url: `${API}/clanfight/list/${req.params.id}?showOptionsCount=true&showDescription=true&showOptions=true`,
                 responseType: 'json',
             }).then(response => {
-                console.log(response);
-                res.json({ polls: response.data.results || [] });
-            });
+                //console.log(response);
+                if (pollid !== null) {
+                    filtered = response.data.results.filter(poll => poll.id == pollid).map((poll, idx) => (
+                        poll.options.sort(function (a, b) { return a.collected - b.collected; }).reverse().splice(0, 1)
+                    ));
+                };
+                res.json({ polls: response.data.results || [], background: filtered || null })
+            })
         }
         catch (error) {
             console.error(error.message);
